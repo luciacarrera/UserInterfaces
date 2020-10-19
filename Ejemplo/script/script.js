f3799e1e80d440f27a18dc8a2e500d722f84d688
lucirace <luciaacarreraa@gmail.com>
2020-10-19 17:45:55 +0200

    Base ejercicio + ejemplo

---
+++ Ejemplo/script/script.js
@@ -0,0 +1,11 @@
+function validateForm(){
+	var email = document.forms["login"]["email"].value;
+	if(email == ""){
+		alert("Please, fill the email");
+		return false;
+	}
+
+	//creating the cookie (sign up)
+	//loading the personal page (login)
+
+}
