a256e4250e5311897d1da7c8c9daa1614e0b63c9
Ignacix <72254021+Ignacix@users.noreply.github.com>
2020-10-27 16:06:28 +0100

    accessing alfea

--- pe02_gr17/Script/script.js
+++ pe02_gr17/Script/script.js
@@ -81,3 +81,30 @@ function showCal(){
         trigger:null
     });
 }
+
+// Function to open Alfea's webpage
+function openAlfea() {
+    var x = document.getElementById("homepage").style.display = "none";
+    var y = document.getElementById("container").style.display = "block";
+}
+
+// Functions to switch between Login and Register
+function register() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register")
+    var z = document.getElementById("btn");
+
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function login() {
+    var x = document.getElementById("login");
+    var y = document.getElementById("register")
+    var z = document.getElementById("btn");
+
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0";
+}
